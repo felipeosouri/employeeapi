8db3817deb72de33be63286973572a26c7b437e8
felipeosouri <jfosorio@outlook.com>
2020-08-05 23:19:54 -0500

    Unit test

--- Tests/EmployeeTest.cs
+++ Tests/EmployeeTest.cs
@@ -1,41 +1,57 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Models;
+using Persistence.Database;
 using Repositories;
+using System.Linq;
 using Tests.Config;
 namespace Tests
 {
     [TestClass]
     public class EmployeeTest
     {
-        private readonly IEmployeeRepository _repository;
-        public EmployeeTest(IEmployeeRepository repository)
+        private IEmployeeRepository _repository;
+        private ApplicationDbContext _context;
+
+        [TestInitialize]
+        public void Init()
         {
-            _repository = repository;
+            _context = ApplicationDbContextInMemory.Get();
+            _repository = new EmployeeRepository(_context);
         }
 
-        [TestMethod]
-        public void GetAllEmployees()
+        private Employee GetInstance(int id, decimal amount = 15)
         {
-            //var context = ApplicationDbContextInMemory.Get();
-
-            //var employeeId = 1;
-            //var name = "Juan Felipe";
-            //var surname = "Osorio Uribe";
-            //var contractType = "Hourly";
-            //var amount = 10;
-
-            //context.Employees.Add(new Employee
-            //{
-            //    EmployeeId = employeeId,
-            //    Name = name,
-            //    Surname = surname,
-            //    ContractType = contractType,
-            //    Amount = amount
-            //});
-
-            //context.SaveChanges();
+            return new Employee
+            {
+                EmployeeId = id,
+                Name = "Juan Felipe",
+                Surname = "Osorio Uribe",
+                ContractType = "Hourly",
+                Amount = amount
+            };
+        }
+        [TestMethod]
+        public void GetAllEmployees_Pass()
+        {
+            _context.Add(GetInstance(1));
+            _context.SaveChanges();
 
             var employees = _repository.GetAll();
+
+            Assert.IsTrue(employees.Count > 0);
+        }
+        [TestMethod]
+        public void GetAllEmployeesMayorThat15()
+        {            
+            _context.Add(GetInstance(2, 20));
+            _context.Add(GetInstance(3, 15));
+            _context.SaveChanges();
+
+            var employees = _repository.GetAll();
+            employees = employees.Where(x => x.Amount == 15).ToList();
+            Assert.IsTrue(employees.Count == 1);
         }
     }
 }
